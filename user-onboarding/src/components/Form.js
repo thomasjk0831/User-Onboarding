30073fe5e4431a617a62c207ec0adeb5a82726de
Thomas Kim <Thomasjk0831@gmail.com>
2020-08-19 09:27:00 -0400

    finished mvp

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const formSchema = yup.object().shape({
     name: yup.string().required("Name is a required field"),
     email: yup.string().email("Must be a valid email address").required("Must include email address"),
-    password: yup.string().required("Must include password"),
+    password: yup.string().min(8, "Must be at least 8 chars").required("Must include password"),
     position: yup.string(),
     terms: yup.boolean().oneOf([true], "Please agree to terms of use")
 })
@@ -26,19 +26,53 @@ export default function Form(){
         position: "",
         terms: "",
     })
+    const [buttonDisabled, setButtonDisabled] = useState(true)
          
     const changeHandler = event => {
-        
-        const value = event.target.type === "checkbox"? event.target.checked : event.target.value
+        event.persist();
+        validate(event)
+        let value = event.target.type === "checkbox"? event.target.checked : event.target.value
        setForm({...form, [event.target.name] : value})
     }
 
+    const validate = e => {
+      let value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+
+      yup
+        .reach(formSchema, e.target.name)
+        .validate(value)
+        .then(valid=> {
+          setError({...error, [e.target.name]: ""})
+        })
+        .catch(err=> {
+          setError({...error, [e.target.name]: err.errors[0]})
+        })
+
+    }
     
+    const submitHandler = (event) => {
+        event.preventDefault()
+        axios.post('https://reqres.in/api/users', form)
+        .then(response => {
+          console.log(response.data)
+          setUsers(response.data)
+        })
+        .catch(err => console.log(err))
+    }
+
+    useEffect(()=> {
+      formSchema.isValid(form)
+      .then(valid => {
+        setButtonDisabled(!valid)
+      })
+    }, [form])
+    
+
 
     return(
         <div>
-          <form>
-              <label forhtml='name'>Name
+          <form onSubmit={submitHandler}>
+              <label htmlFor='name'>Name
                 <input 
                   id='name' 
                   name='name' 
@@ -47,8 +81,11 @@ export default function Form(){
                   placeholder='Enter name'
                   onChange={changeHandler}
                   />
+                  {error.name.length > 0 ? (
+                    <p>{error.name}</p>
+                  ): null}
               </label>
-              <label forhtml='email'>Email
+              <label htmlFor='email'>Email
                 <input 
                   id='email' 
                   name='email' 
@@ -57,20 +94,26 @@ export default function Form(){
                   placeholder='Enter email'
                   onChange={changeHandler}
                   />
+                  { error.email.length > 0 ? (
+                    <p>{error.email}</p>
+                  ) : null }
               </label>
-              <label forhtml='password'>Password
+              <label htmlFor='password'>Password
                 <input 
                   id='password' 
                   name='password' 
                   type="password"
                   value={form.password} 
-                  placeholder='Enter email'
+                  placeholder='Enter password'
                   onChange={changeHandler}
                   />
+                  { error.password.length > 0 ? (
+                    <p>{error.password}</p>
+                  ) : null }
               </label>
               
             <label htmlFor="position">
-              What would you like to help with?
+              What is the person's position?
                <select
                  value={form.position}
                  name="position"
@@ -85,17 +128,20 @@ export default function Form(){
                </select>
         
             </label>
-            <label htmlfor="terms">
+            <label htmlFor="terms"> I agree to the terms of use
                 <input 
                   id="terms" 
                   name="terms" 
                   type="checkbox" 
                   checked={form.terms} 
                   onChange={changeHandler} />
+                   { error.terms.length > 0 ? (
+                    <p>{error.terms}</p>
+                  ) : null }
             </label>
 
-                     
-
+            <button disabled={buttonDisabled} type="submit">Add User</button>
+            <pre>{JSON.stringify(users, null, 2)}</pre>
           </form>
         </div>
     )
